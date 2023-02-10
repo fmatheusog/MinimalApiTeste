a1d6ba477ffb55750ad98a97e39749aa0d6ab8ab
Felipe Mori <felipe.mori@onfriday.com.br>
2023-02-10 10:04:41 -0300

    feat: CRUD fornecedor

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApiTeste.Data;
+using MinimalApiTeste.Models;
+using MiniValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -16,29 +22,75 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/fornecedor", async (MinimalContextDb context) => await context.Fornecedores.ToListAsync())
+    .WithName("GetFornecedor")
+    .WithTags("Fornecedor");
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/fornecedor/{id}", async (Guid id, MinimalContextDb context) =>
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor
+            ? Results.Ok(fornecedor)
+            : Results.NotFound())
+    .Produces<Fornecedor>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetFornecedorById")
+    .WithTags("Fornecedor");
+
+app.MapPost("/fornecedor", async (MinimalContextDb context, Fornecedor fornecedor) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Add(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        // ? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor)
+        ? Results.CreatedAtRoute("GetFornecedorById", new { id = fornecedor.Id }, fornecedor)
+        : Results.BadRequest("Houve um problema ao salvar o registro");
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+.Produces<Fornecedor>(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("PostFornecedor")
+.WithTags("Fornecedor");
+
+app.MapPut("/fornecedor", async (Guid id, MinimalContextDb context, Fornecedor fornecedor) =>
+{
+    var fornecedorBanco = await context.Fornecedores.FindAsync(id);
+    if (fornecedor is null) return Results.NotFound(); 
+
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Update(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao salvar o registro");
+})
+.Produces<Fornecedor>(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status404NotFound)
+.WithName("PutFornecedor")
+.WithTags("Fornecedor");
+
+app.MapDelete("/fornecedor/{id}", async (Guid id, MinimalContextDb context) =>
+{
+    var fornecedor = await context.Fornecedores.FindAsync(id);
+    if (fornecedor is null) return Results.NotFound();
+
+    context.Fornecedores.Remove(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest();
+})
+.Produces<Fornecedor>(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status404NotFound)
+.WithName("DeleteFornecedor")
+.WithTags("Fornecedor");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
